f7bfe6ee104991e5b4dcaf16b586d63056d58d94
KimTibber <atstudio@daum.net>
2022-04-10 16:24:37 +0900

    프로그램 내에서 키음 변경 지원 추가

---
+++ DjmaxTyping/ConfigWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace Typing
+{
+    /// <summary>
+    /// ConfigWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        Configure cfg = new Configure();
+        public delegate void SendMessageDlg(string msg);
+        public event SendMessageDlg SendMsg;
+
+        public ConfigWindow()
+        {
+            InitializeComponent();
+
+            // INI 파일에 있는 내용으로 셋팅
+            PlayFileTextBox.Text = (string) cfg.Get("audiopath");
+        }
+ 
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void FindFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlgOpenFile = new OpenFileDialog();
+            dlgOpenFile.Filter = "미디어 파일 (*.mp3, *.wav)|*.mp3;*.wav|mp3 (*.mp3)|*.mp3|wav (*.wav)|*.wav";
+
+            if (dlgOpenFile.ShowDialog().ToString().Equals("OK"))
+            {
+                PlayFileTextBox.Text = dlgOpenFile.FileName;
+            }
+        }
+
+        private void SaveCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoSave(PlayFileTextBox.Text);
+            this.DialogResult = true;
+        }
+
+        private void DoSave(string fileName)
+        {
+            cfg.Put("audioPath", fileName);
+            SendMsg(fileName);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoSave(PlayFileTextBox.Text);
+            System.Windows.Forms.MessageBox.Show("설정이 저장되었습니다!");
+        }
+    }
+}
---
+++ DjmaxTyping/Configure.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Typing
+{
+    class Configure
+    {
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        private string configFileName = $"{Path.Combine(Directory.GetCurrentDirectory(), "typing.ini")}";
+        private string defaultSectionName = "DJMAX_TYPING";
+
+        public enum ParseDateType
+        {
+            BOOL,
+            INT,
+            STRING
+        }
+
+        public Configure(string fileName=null, string sectionName=null)
+        {
+            if (fileName != null) this.configFileName = fileName;
+            if (sectionName != null) this.defaultSectionName = sectionName;
+        }
+
+        public object Get(string keyName, string defaultValue="", ParseDateType type=ParseDateType.STRING, string sectionName=null)
+        {
+            /*
+             * INI 파일의 키에 대응되는 값을 가져온다
+             */
+
+            StringBuilder sb = new StringBuilder(defaultValue);
+
+            // ini 읽기
+            GetPrivateProfileString((sectionName == null ? this.defaultSectionName : sectionName), keyName, defaultValue, sb, (sb.MaxCapacity + 1), this.configFileName);
+
+            string tempVal = sb.ToString(); // 임시값
+
+            object resultVal = null; // 최종 반환값
+
+            // 원하는 자료형에 따라 값 변환
+            switch (type)
+            {
+                case ParseDateType.BOOL:
+                    resultVal = (bool)(tempVal.Equals("true") || tempVal.Equals("1"));
+                    break;
+                case ParseDateType.INT:
+                    resultVal = (int)int.Parse(tempVal);
+                    break;
+                case ParseDateType.STRING:
+                default:
+                    resultVal = (string)tempVal;
+                    break;
+            }
+
+            return resultVal;
+        }
+
+        public void Put(string keyName, string value, string sectionName=null)
+        {
+            WritePrivateProfileString((sectionName == null ? this.defaultSectionName : sectionName), keyName, value, this.configFileName);
+        }
+    }
+}
